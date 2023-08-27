fbe2e679425e6048942fc8134923050ed95284ec
brainless <monkeydjiotaro+brainless@gmail.com>
2023-08-27 11:53:12 +0700

    [js] try out shebang in nodejs

--- vo_tri.js
+++ vo_tri.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readline.question(`Enter a number:`, (user_hand) => {
+readline.question("Enter a number: ", (user_hand) => {
   if (Number(user_hand) < 10) {
     conosle.log("Eh...");
   } else {
