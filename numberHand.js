d969f1716b38fd6e3e914d3bb9ce6a0ac7c69f70
brainless <monkeydjiotaro+brainless@gmail.com>
2023-08-27 12:29:42 +0700

    [js] remove an unnecessary comment

--- numberHand.js
+++ numberHand.js
@@ -7,7 +7,6 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-// I have ridiculously implemented such code countless times...
 const randRange = (min = 0, max = 10) => Math.random() * (max - min) + min;
 // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 // const waitRandomly = (secs) => delay(randRange(secs * 1e3));
