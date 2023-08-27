b7bd06df3f93467193471d33752f4a65011966df
brainless <monkeydjiotaro+brainless@gmail.com>
2023-08-27 13:32:30 +0700

    [python] get user hand from first argv

--- number_hand.py
+++ number_hand.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
 import time
+from sys import argv
 from random import randrange
 
-user_hand = float(input('Enter a number: '))
+if len(argv) == 2:
+    user_hand = float(argv[1])
+    print(f'Got a number: [{user_hand}]')
+else:
+    user_hand = float(input('Enter a number: '))
 
-print("and Bot hand is... ", end=" ")
+print('and Bot hand is...', end=' ')
 
 time.sleep(randrange(0, 3))
 bot_hand = randrange(0, user_hand * 2)
 
-print(f'**{bot_hand}**')
+print(f'[{bot_hand}]')
 
 if float(user_hand) <= bot_hand:
     print('Eh...')
