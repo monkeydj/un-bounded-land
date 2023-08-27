93615a0256853e7cb5e0de2fd7ddfe2a583b3bea
brainless <monkeydjiotaro+brainless@gmail.com>
2023-08-27 12:24:01 +0700

    [js] what is the meaning of naming things anyway

--- numberHand.js
+++ numberHand.js
@@ -11,11 +11,11 @@ const randRange = (min = 0, max = 10) => Math.random() * (max - min) + min;
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const waitRandomly = (secs) => delay(randRange(secs * 1e3));
 
-const generateBotHand = (centerPoint = 10) => randRange(0, centerPoint);
+const generateBotHandByCentre = (aNumber = 10) => randRange(0, aNumber * 2);
 
 readline.question("Enter a number: ", (userInputs) => {
   const userHand = Number(userInputs);
-  const botHand = generateBotHand(userHand * 2);
+  const botHand = generateBotHandByCentre(userHand);
 
   process.stdout.write("and Bot hand is... ");
   waitRandomly(3).then(() => console.log(`**${botHand}**`));
