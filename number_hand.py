d53d9c9f4041e8206804c08bb54049dcc3b157d5
brainless <monkeydjiotaro+brainless@gmail.com>
2023-08-27 13:35:28 +0700

    [python] another ridiculed action of double quotes

--- number_hand.py
+++ number_hand.py
@@ -6,18 +6,21 @@ from random import randrange
 
 if len(argv) == 2:
     user_hand = float(argv[1])
-    print(f'Got a number: [{user_hand}]')
+    print(f"Got a number: [{user_hand}]")
 else:
-    user_hand = float(input('Enter a number: '))
+    user_hand = float(input("Enter a number: "))
 
-print('and Bot hand is...', end=' ')
+print("and Bot hand is...", end=" ")
 
 time.sleep(randrange(0, 3))
 bot_hand = randrange(0, user_hand * 2)
 
-print(f'[{bot_hand}]')
+print(f"[{bot_hand}]")
 
 if float(user_hand) <= bot_hand:
-    print('Eh...')
+    print("Eh...")
 else:
-    print('!!! You Win Luckily !!!')
+    print("!!! You Win Luckily !!!")
+
+if __name__ == "__main__":
+    pass
