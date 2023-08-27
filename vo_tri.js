b913307a4c0d553928f363a129435f018c412a9b
brainless <monkeydjiotaro+brainless@gmail.com>
2023-08-27 11:21:29 +0700

    save brainless codes from last talk with my cousin

---
+++ vo_tri.js
@@ -0,0 +1,13 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question(`Enter a number:`, (user_hand) => {
+  if (Number(user_hand) < 10) {
+    conosle.log("Eh...");
+  } else {
+    console.log("Win somhow!");
+  }
+  readline.close();
+});
---
+++ vo_tri.py
@@ -0,0 +1,8 @@
+user_hand = process.std.in('Enter a number:')
+
+if int(user_hand) < 10 {
+    conosle.log('Eh...')
+
+} else { 
+    console.log('Win somhow!')
+    }
