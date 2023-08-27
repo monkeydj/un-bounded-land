c2f4ed96c1679f5c711353318b81f2aa3afaa516
brainless <monkeydjiotaro+brainless@gmail.com>
2023-08-27 12:21:19 +0700

    [js] ridiculously improve to have same logic as python but become miserable

--- numberHand.js
+++ numberHand.js
@@ -5,15 +5,25 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
+// I have ridiculously implemented such code countless times...
+const randRange = (min = 0, max = 10) => Math.random() * (max - min) + min;
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const waitRandomly = (secs) => delay(randRange(secs * 1e3));
+
+const generateBotHand = (centerPoint = 10) => randRange(0, centerPoint);
+
 readline.question("Enter a number: ", (userInputs) => {
-  console.log(checkHands(userInputs));
+  const userHand = Number(userInputs);
+  const botHand = generateBotHand(userHand * 2);
+
+  process.stdout.write("and Bot hand is... ");
+  waitRandomly(3).then(() => console.log(`**${botHand}**`));
+
+  console.log(checkHands(userHand, botHand));
+
   readline.close(); // must end i/o blocking
 });
-
-function generateBotHand(min = 0, max = 10) {
-  return Math.random() * (max - min) + min;
-  // I have ridiculously implemented such code countless times...
-}
 
 function checkHands(userHand, botHand = generateBotHand()) {
   return userHand < botHand ? "Eh..." : "[!!] Winner [!!]";
