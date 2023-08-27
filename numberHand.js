eba0c9519ec787e7daad58d4e9f27f63678f157f
brainless <monkeydjiotaro+brainless@gmail.com>
2023-08-27 12:29:05 +0700

    [js] implement waitRandomlySync

--- numberHand.js
+++ numberHand.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const { execSync } = require("child_process");
+
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -7,9 +9,9 @@ const readline = require("readline").createInterface({
 
 // I have ridiculously implemented such code countless times...
 const randRange = (min = 0, max = 10) => Math.random() * (max - min) + min;
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-const waitRandomly = (secs) => delay(randRange(secs * 1e3));
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// const waitRandomly = (secs) => delay(randRange(secs * 1e3));
+const waitRandomlySync = (secs) => execSync(`sleep ${randRange(secs)}`);
 
 const generateBotHandByCentre = (aNumber = 10) => randRange(0, aNumber * 2);
 
@@ -18,8 +20,9 @@ readline.question("Enter a number: ", (userInputs) => {
   const botHand = generateBotHandByCentre(userHand);
 
   process.stdout.write("and Bot hand is... ");
-  waitRandomly(3).then(() => console.log(`**${botHand}**`));
+  waitRandomlySync(3);
 
+  console.log(`**${botHand}**`);
   console.log(checkHands(userHand, botHand));
 
   readline.close(); // must end i/o blocking
