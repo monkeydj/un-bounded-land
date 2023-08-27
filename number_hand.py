9f5481e50d444b1507559176d36fc4471ed6ab14
brainless <monkeydjiotaro+brainless@gmail.com>
2023-08-27 13:41:35 +0700

    [python] refactor to be formalized according to name-main idiom

--- number_hand.py
+++ number_hand.py
@@ -4,23 +4,41 @@ import time
 from sys import argv
 from random import randrange
 
-if len(argv) == 2:
-    user_hand = float(argv[1])
-    print(f"Got a number: [{user_hand}]")
-else:
-    user_hand = float(input("Enter a number: "))
 
-print("and Bot hand is...", end=" ")
+def getUserHand():
 
-time.sleep(randrange(0, 3))
-bot_hand = randrange(0, user_hand * 2)
+    if len(argv) == 2:
+        user_hand = float(argv[1])
+        print(f"Got a User hand: [{user_hand}]")
+    else:
+        user_inputs = input("Enter a number for your hand: ")
+        user_hand = float(user_inputs)
 
-print(f"[{bot_hand}]")
+    return user_hand
 
-if float(user_hand) <= bot_hand:
-    print("Eh...")
-else:
-    print("!!! You Win Luckily !!!")
+
+def getBotHand(user_hand=10):
+
+    print("and Bot hand is...", end=" ")
+
+    time.sleep(randrange(0, 3))
+    bot_hand = randrange(0, user_hand * 2)
+
+    print(f"[{bot_hand}]")
+
+    return bot_hand
+
+
+def start():
+
+    user_hand = getUserHand()
+    bot_hand = getBotHand(user_hand)  # cheat purposefully
+
+    if float(user_hand) <= bot_hand:
+        print("Eh...")
+    else:
+        print("!!! You Win Luckily !!!")
+
 
 if __name__ == "__main__":
-    pass
+    start()
